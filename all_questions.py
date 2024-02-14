6e75201677734849756a7a7d4f5f8037890d3ed6
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-02-14 18:39:16 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -122,7 +122,7 @@ def question2():
     answer["(b) y <= 0.6"] = 0.385102388
 
     # choose one of 'x=0.2', 'x=0.7', or 'x=0.6'
-    answer["(c) attribute"] = 'x=0.6'
+    answer["(c) attribute"] = 'x=0.2'
 
     # Use the Binary Tree structure to construct the tree
     # Answer is an instance of BinaryTree
