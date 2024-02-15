8e1856ac8fd5b45412b0f6955ef85cc3ee57ab70
jeremiahbagadion <131178410+jeremiahbagadion@users.noreply.github.com>
2024-02-15 15:09:09 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -117,8 +117,8 @@ def question2():
     answer["(a) entropy_entire_data"] = 1.425364
     # Infogain
     answer["(b) x < 0.2"] = 0.5637042
-    answer["(b) x <= 0.7"] = 0.3022116
-    answer["(b) y <= 0.6"] = 0.385102388
+    answer["(b) x < 0.7"] = 0.3022116
+    answer["(b) y < 0.6"] = 0.385102388
     # choose one of 'x=0.2', 'x=0.7', or 'x=0.6'
     answer["(c) attribute"] = 'x=0.2'
     # Use the Binary Tree structure to construct the tree
